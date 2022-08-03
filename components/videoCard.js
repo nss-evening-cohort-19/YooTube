f082ca973d4fefcc036f3a46c8b223d984bb0d7a
Brett Hughes <twofivinclimber@gmail.com>
2022-08-03 16:21:07 -0500

    video card created

--- components/videoCard.js
+++ components/videoCard.js
@@ -1,21 +1,21 @@
 import { React } from 'react';
-import { Card, Button, Image } from 'react-bootstrap';
+import { Card, Image } from 'react-bootstrap';
 import YouTube from 'react-youtube';
 import PropTypes from 'prop-types';
 
+// eslint-disable-next-line react/prop-types
 function VideoCard({ obj }) {
   return (
     <div>
-      <Card style={{ width: '20rem' }}>
-        <YouTube videoId={obj.videoId} />
-        <Card.Body>
-          <Card.Title>{obj.title}</Card.Title>
-          <Card.Text>{obj.description}</Card.Text>
-          <Card.Text>Likes: {obj.likes}</Card.Text>
-          <Image src={obj.creatorImage} />
-          <Card.Text>{obj.creatorName}</Card.Text>
-          <Button variant="primary">Edit</Button>
-          <Button variant="danger">Delete</Button>
+      <Card className="videoCard">
+        <YouTube opts={{ height: '160', width: '280' }} videoId={obj.videoId} />
+        <Card.Body className="videoCardBody">
+          <Image className="vidCardCreatorImage" src={obj.creatorImage} />
+          <div className="videoCardTextdiv">
+            <Card.Title className="vidCardTitle">{obj.title}</Card.Title>
+            <Card.Text className="vidCardCreatorName">{obj.creatorName}</Card.Text>
+            <Card.Text className="vidCardLikes">Likes: {obj.likes}</Card.Text>
+          </div>
         </Card.Body>
       </Card>
     </div>
