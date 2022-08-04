7d887b7b7539d5b9ebb6b02d428894a4ba2d2d08
Brett Hughes <twofivinclimber@gmail.com>
2022-08-04 12:18:35 -0500

    added route from card to /video/[firebaseKey] page

--- components/videoCard.js
+++ components/videoCard.js
@@ -1,18 +1,22 @@
+/* eslint-disable react/void-dom-elements-no-children */
 import { React } from 'react';
 import { Card, Image } from 'react-bootstrap';
 import YouTube from 'react-youtube';
 import PropTypes from 'prop-types';
+import Link from 'next/link';
 
 // eslint-disable-next-line react/prop-types
-function VideoCard({ obj }) {
+function VideoCard({ obj, opts }) {
   return (
     <div>
       <Card className="videoCard">
-        <YouTube opts={{ height: '160', width: '280' }} videoId={obj.videoId} />
+        <YouTube opts={opts} videoId={obj.videoId} />
         <Card.Body className="videoCardBody">
           <Image className="vidCardCreatorImage" src={obj.creatorImage} />
           <div className="videoCardTextdiv">
-            <Card.Title className="vidCardTitle">{obj.title}</Card.Title>
+            <Link href={`/video/${obj.videoFirebaseKey}`} passHref>
+              <Card.Title className="vidCardTitle">{obj.title}</Card.Title>
+            </Link>
             <Card.Text className="vidCardCreatorName">{obj.creatorName}</Card.Text>
             <Card.Text className="vidCardLikes">Likes: {obj.likes}</Card.Text>
           </div>
