0abaf5661825ccd1db5eb5e3cc51f383c15ce7c9
Christian Black <blackl3@gmail.com>
2022-08-04 19:18:42 -0500

    Saving changes

--- api/commentData.js
+++ api/commentData.js
@@ -2,3 +2,11 @@ import axios from 'axios';
 import { clientCredentials } from '../utils/client';
 
 const dbUrl = clientCredentials.databaseURL;
+
+const getAllComments = (videoFirebaseKey) => new Promise((resolve, reject) => {
+  axios.get(`${dbUrl}/comments.json?orderBy="videoFirebaseKey"&equalTo="${videoFirebaseKey}"`)
+    .then((response) => resolve(Object.values(response.data)))
+    .catch(reject);
+});
+
+export { getAllComments };
