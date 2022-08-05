6744a8b6f5840b086424446c70a362dd613be93e
Brett Hughes <twofivinclimber@gmail.com>
2022-08-04 20:11:48 -0500

    need to merge

--- pages/video/[firebaseKey].js
+++ pages/video/[firebaseKey].js
@@ -1,22 +1,45 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
+import YouTube from 'react-youtube';
+import { Card, Button, Image } from 'react-bootstrap';
+import { getSingleVideo } from '../../api/videoData';
 
 function ViewVideo() {
-  // const [video, setVideo] = useState({});
+  const [video, setVideo] = useState({});
   const router = useRouter();
   const { firebaseKey } = router.query;
 
   const getTheVideo = () => {
-    // getSingleVideo(firebaseKey).then(setVideo);
+    getSingleVideo(firebaseKey).then(setVideo);
   };
 
   useEffect(() => {
     getTheVideo();
-  }, []);
+  }, [video]);
 
   return (
-    <h1>{firebaseKey}</h1>
+    <div>
+      <div className="viewVideoDiv">
+        <Card className="viewVideCard" style={{ width: '75%' }}>
+          <YouTube opts={{ height: '390', width: '640' }} videoId={video.videoId} />
+          <Card.Body>
+            <div className="videoTextdiv">
+              <Image className="vidCardCreatorImage" src={video.creatorImage} />
+              <Card.Title className="vidTitle">{video.title}</Card.Title>
+              <Card.Text className="vidCreatorName">{video.creatorName}</Card.Text>
+              <Card.Text className="vidDate">{video.date}</Card.Text>
+            </div>
+            <div className="vidButtonDiv">
+              <Button>👍</Button>
+              <Card.Text className="vidLikes">Likes: {video.likes}</Card.Text>
+              <Button>👎</Button>
+            </div>
+          </Card.Body>
+        </Card>
+      </div>
+    </div>
+
   );
 }
 export default ViewVideo;
