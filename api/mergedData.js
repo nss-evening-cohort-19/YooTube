6fe68718f2104de0ddb12d2904dd1c5e53f7d014
Brett Hughes <twofivinclimber@gmail.com>
2022-08-06 10:32:34 -0500

    commit to get code

---
+++ api/mergedData.js
@@ -0,0 +1,11 @@
+// import axios from 'axios';
+// import { clientCredentials } from '../utils/client';
+
+// const dbUrl = clientCredentials.databaseURL;
+
+// const getVideoAndComments = (firebaseKey) => new Promise((resolve, reject) => {
+//   axios.get(`${dbUrl}/videos/${firebaseKey}`)
+//     .then((videoObj) => {
+//       axios.get(`${dbUrl}/comments`)
+//     })
+// })
