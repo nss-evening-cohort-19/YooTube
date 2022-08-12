a46bffd48fbb1be0381346be274cf3cfb886d178
Lesley Keyes <lesleyjkeyes@gmail.com>
2022-08-11 19:10:37 -0500

    channel view

--- pages/channel/[uid].js
+++ pages/channel/[uid].js
@@ -2,19 +2,30 @@ import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import { Card } from 'react-bootstrap';
 import VideoCard from '../../components/videoCard';
+import { getUser } from '../../api/userData';
 import { getUserVideos } from '../../api/videoData';
 
 function UserVideos() {
   const [videos, setVideos] = useState([]);
+  const [user, setUser] = useState({});
   const router = useRouter();
 
-  const getChannelVideos = () => {
-    getUserVideos(videos.uid).then(setVideos);
-    console.warn(videos);
+  const getThisUser = () => {
+    getUser(user.uid).then(setUser);
+    console.warn(user);
+  };
+
+  const getTheseVideos = () => {
+    getUserVideos(user.uid).then(setVideos);
   };
 
   useEffect(() => {
-    getChannelVideos();
+    getThisUser();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    getTheseVideos();
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
@@ -23,14 +34,14 @@ function UserVideos() {
       <div className="userChannel">
         <Card style={{ width: '18rem' }}>
           <Card.Body>
-            <Card.Title>name here</Card.Title>
+            <Card.Title>{user.displayName}</Card.Title>
           </Card.Body>
         </Card>
       </div>
       <h2>User Videos</h2>
       <div className="d-flex flex-wrap">
         {videos?.map((video) => (
-          <VideoCard key={video.videoFirebaseKey} obj={video} opts={{ height: '160', width: '280' }} onUpdate={getChannelVideos} router={router.asPath} />
+          <VideoCard key={video.videoFirebaseKey} obj={video} opts={{ height: '160', width: '280' }} onUpdate={getTheseVideos} router={router.asPath} />
         ))}
       </div>
 
